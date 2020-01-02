e3064e0f5229af245ddb1ea2e7620c2aa441f13c
edesmontils <emmanuel.desmontils@univ-nantes.fr>
2020-01-02 09:02:31 +0100

    ed

--- pyPeNet.py
+++ pyPeNet.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.7 
+#!/usr/bin/env python3.7
 # coding: utf8
 
 
@@ -14,5 +14,31 @@ class Transition(object):
 
 
 class Arc(object):
-    def __init__(self, place, arc, poids=1):
-        pass
+    def __init__(self, poids=1):
+        assert poids >= 0, "Erreur sur le poids"
+        self.poids = poids
+        self.source = None
+        self.cible = None
+
+
+class ArcPT(Arc):
+    def __init__(self, place, transition, poids=1):
+        super(poids)
+        self.source = place
+        self.cible = transition
+
+
+class ArcTP(Arc):
+    def __init__(self, transition, place, poids=1):
+        super(poids)
+        self.cible = place
+        self.source = transition
+
+
+
+#==================================================
+#==================================================
+#==================================================
+
+if __name__ == '__main__':
+	print('main de pyPeNet.py')
