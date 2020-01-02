8d5a9eb1294186183ce66e146b2d96ff6d886bb2
edesmontils <emmanuel.desmontils@univ-nantes.fr>
2020-01-02 09:47:36 +0100

    ed

--- pyPeNet.py
+++ pyPeNet.py
@@ -7,10 +7,16 @@ class Place(object):
         self.name = name
         self.contains = jetons
 
+    def __str__(self):
+        return self.name
+
 
 class Transition(object):
     def __init__(self, name):
         self.name = name
+
+    def __str__(self):
+        return self.name
 
 
 class Arc(object):
@@ -20,25 +26,42 @@ class Arc(object):
         self.source = None
         self.cible = None
 
+    def __str__(self):
+        return self.source+"-"+self.poids+"->"+self.cible
+
 
 class ArcPT(Arc):
     def __init__(self, place, transition, poids=1):
-        super(poids)
+        super().__init__(poids)
+        assert isinstance(place, Place), "place n'est pas une Place"
+        assert isinstance(
+            transition, Transition), "transition n'est pas une Transition"
         self.source = place
         self.cible = transition
 
 
 class ArcTP(Arc):
     def __init__(self, transition, place, poids=1):
-        super(poids)
+        super().__init__(poids)
+        assert isinstance(place, Place), "place n'est pas une Place"
+        assert isinstance(
+            transition, Transition), "transition n'est pas une Transition"
         self.cible = place
         self.source = transition
 
 
+class PeNet(object):
+    pass
 
-#==================================================
-#==================================================
-#==================================================
+
+# ==================================================
+# ==================================================
+# ==================================================
+
 
 if __name__ == '__main__':
-	print('main de pyPeNet.py')
+    print('main de pyPeNet.py')
+    p1 = Place("p1")
+    t1 = Transition("t1")
+    a = ArcPT(p1, t1)
+    print(a)
