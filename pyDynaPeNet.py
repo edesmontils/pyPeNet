d2a3063906ae72b3ac4d18c24bd1a0f6bf0c6b73
edesmontils <emmanuel.desmontils@univ-nantes.fr>
2020-01-09 22:39:57 +0100

    ed

--- pyDynaPeNet.py
+++ pyDynaPeNet.py
@@ -2,12 +2,48 @@
 # coding: utf8
 
 """
-    Bibliothèque pour représenter les Réseaux de Pétri (RdP) pour exécuter sous EV3.
+    Bibliothèque pour représenter les Réseaux de Pétri (RdP) à exécuter.
     TODO :
-    - EV3PeNet
+    - gestion des événements clavier
+    - gestion des événements souris (?)
 """
 from pyPeNet import *
+# https://pythonhosted.org/pynput/keyboard.html & https://pythonhosted.org/pynput/mouse.html
+from pynput.keyboard import Key, Listener
 
+from multiprocessing import Manager
+
+# ==================================================
+# ==================================================
+# ==================================================
+
+# m = Manager()
+keysDict = dict()
+
+
+def runListener():
+    print('Start KeyboardListener')
+    try:
+        with Listener(on_press=on_press, on_release=on_release) as listener:
+            listener.join()
+    except KeyboardInterrupt:
+        print('End KeyboardListener')
+
+
+def on_press(key):
+    pass  # print('{0} pressed'.format(key))
+
+
+def on_release(key):
+    k = str(key)
+    keysDict[k] = keysDict.get(k, 0) + 1
+    print(type(k))
+    if key == Key.esc:
+        # Stop listener
+        return False
+
+
+# ==================================================
 # ==================================================
 # ==================================================
 
@@ -51,17 +87,20 @@ class StdoutDisplayEvent(DisplayEvent):
 
 
 class InEvent(Event):
-    def __init__(self, port=None):
+    def __init__(self):
         super(InEvent, self).__init__()
-        self.port = port
+
+    def done(self) :
+        return False
 
 # ==================================================
 
 
 class Sensor(InEvent):
-    def __init__(self, port):
+    def __init__(self, port=None):
         super(Sensor, self).__init__(port)
         assert port != None, "[Sensor init] bad port value"
+        self.port = port
         self.change = False
 
     def raised(self):
@@ -77,9 +116,11 @@ class ButtonEvent(InEvent):
     def __init__(self):
         super(ButtonEvent, self).__init__()
 
+
 class KeyboardEvent(InEvent):
-    def __init__(self):
+    def __init__(self, c):
         super(KeyboardEvent, self).__init__()
+        self.c = c
 
 
 # ==================================================
@@ -88,7 +129,7 @@ class KeyboardEvent(InEvent):
 
 
 class DynaPeNet(PeNet_I):
-    def __init__(self, ):
+    def __init__(self):
         super(DynaPeNet, self).__init__()
 
     def connect(self, fl):
@@ -104,6 +145,13 @@ class DynaPeNet(PeNet_I):
         super().load(P, T, A, W, M0)
         self.F = F
 
+    def estDeclanchable(self, t):
+        return (self.UeT[t] <= self.Mi).all()
+
+    def declancher(self, t):
+        self.v_count[t] += 1
+        self.Mi = self.Mi + self.UT[t]
+
     def next(self):
         t = super().next()
         a = self.F[t]
@@ -111,28 +159,33 @@ class DynaPeNet(PeNet_I):
             a.do()
         return t
 
-
     def run(self):
         t = -1
-        try :
+        try:
             while(t != None):
-                t = self.next()   
+                t = self.next()
+            print("RdP bloqué !")
         except KeyboardInterrupt:
             print("Fin du RdP par interruption:")
+        finally:
             print(self.Mi)
+            print(self.v_count)
 
 
 # ==================================================
 # ==================================================
 # ==================================================
+
+
 if __name__ == '__main__':
     rdp2 = DynaPeNet()
     rdp2.load(("p1", "p2"), ("t1", "t2"), (("p1", "t1"), ("t1", "p2"),
                                            ("p2", "t2"), ("t2", "p1")),
-              (1, 1, 1, 1),  (1, 1), (StdoutDisplayEvent("t1 !"), StdoutDisplayEvent("t2 !")))
+              (1, 1, 1, 1),  (1, 1), (None, None))
 
-    # for i in range(15):
-    #     rdp2.next()
-    #     print(rdp2.Mi)
-
+    l = Listener(on_press=on_press, on_release=on_release)
+    l.start()
     rdp2.run()
+    l.stop()
+    for (i, v) in keysDict.items():
+        print(v)
